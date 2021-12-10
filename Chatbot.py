5c6e2c763bc87df6036bb971d9742be8f4f1e0d8
Shuba13 <92310393+Shuba13@users.noreply.github.com>
2021-12-10 21:17:20 +0530

    Create Chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,5 @@
+print("Hi, I'm the Chiti(Created by Subhaharini) and I like to chat\nPlease type lowercase English language to start a conversation. Type quit to leave ")
+#Create Chat Bot
+chat = Chat(pairs, reflections)
+#Start conversation
+chat.converse()
